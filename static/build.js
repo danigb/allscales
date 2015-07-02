2c2ab328f833339192708ddeb3a61f74e2823256
danigb <danigb@gmail.com>
2015-07-02 18:42:12 +0200

    Use metalsmith

--- static/build.js
+++ static/build.js
@@ -1,6 +1,20 @@
+var metalsmith = require('metalsmith')
 require('node-jsx').install()
-
 var React = require('react/addons')
+
+metalsmith(__dirname)
+  .destination('./output')
+  .use(allscales('index.html'))
+  .build(function (err) {
+    if (err) throw err
+  })
+
+function allscales (filename) {
+  return function (files, metalsmith, done) {
+    files[filename] = { contents: new Buffer('hola!')}
+  }
+}
+
 var all = require('./allscales.js')()
 var AllScalesApp = React.createFactory(require('./components/AllScales'))
 
